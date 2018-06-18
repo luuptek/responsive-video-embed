afc9578f56bee2c26403e7c18b805793e4e43bca
Timo Leppanen <timo@luuptek.fi>
2018-06-18 13:17:44 +0300

    update core file styling

--- wrve.php
+++ wrve.php
@@ -9,23 +9,22 @@
  */
 
 /**
-  * Security Note:
-  * Consider blocking direct access to your plugin PHP files by adding the following line at the top of each of them,
-  * or be sure to refrain from executing sensitive standalone PHP code before calling any WordPress functions.
-  */
+ * Security Note:
+ * Consider blocking direct access to your plugin PHP files by adding the following line at the top of each of them,
+ * or be sure to refrain from executing sensitive standalone PHP code before calling any WordPress functions.
+ */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 /**
-  * Class to build the whole plugin
-  */
+ * Class to build the whole plugin
+ */
 class Rve {
 
 	protected static $instance = null;
 
- 	function __construct()
- 	{
+	function __construct() {
 		add_action( 'init', [ $this, 'initializeHooks' ] );
- 	}
+	}
 
 	public static function getInstance() {
 		if ( self::$instance == null ) {
@@ -40,7 +39,7 @@ class Rve {
 		add_action( 'wp_enqueue_scripts', [ $this, 'getStyles' ] );
 		add_shortcode( 'rve', [ $this, 'embedShortcode' ] );
 		add_action( 'admin_head', [ $this, 'registerTinyMCEButtons' ] );
-		add_filter('embed_oembed_html', [ $this, 'registerEmbedHtml' ], 99, 4);
+		add_filter( 'embed_oembed_html', [ $this, 'registerEmbedHtml' ], 99, 4 );
 	}
 
 	/**
@@ -54,8 +53,8 @@ class Rve {
 	 * Create the actual shortcode
 	 */
 	public function embedShortcode( $atts, $content = null ) {
-		$src = isset( $atts['src'] ) ? $atts['src']: '';
-		$ratio = isset( $atts['ratio'] ) && ( $atts['ratio'] == '16by9' || $atts['ratio'] == '4by3' ) ? $atts['ratio']: '16by9';
+		$src    = isset( $atts['src'] ) ? $atts['src'] : '';
+		$ratio  = isset( $atts['ratio'] ) && ( $atts['ratio'] == '16by9' || $atts['ratio'] == '4by3' || $atts['ratio'] == '21by9' || $atts['ratio'] == '1by1' ) ? $atts['ratio'] : '16by9';
 		$markUp = '';
 
 		$markUp = <<<EOT
@@ -72,33 +71,35 @@ EOT;
 	 */
 	public function registerTinyMCEButtons() {
 		// check user permissions
-    if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
 			return;
-    }
+		}
 
-    if ( get_user_option('rich_editing') == 'true') {
-        add_filter( 'mce_external_plugins', [ $this, 'setRveButtonJs' ] );
-        add_filter( 'mce_buttons', [ $this, 'registerButtons' ] );
-    }
+		if ( get_user_option( 'rich_editing' ) == 'true' ) {
+			add_filter( 'mce_external_plugins', [ $this, 'setRveButtonJs' ] );
+			add_filter( 'mce_buttons', [ $this, 'registerButtons' ] );
+		}
 	}
 
 	public function setRveButtonJs() {
 		$plugin_array['rve_button'] = plugins_url( 'js/rve-button.min.js', __FILE__ );
-    return $plugin_array;
+
+		return $plugin_array;
 	}
 
-	public function registerButtons($buttons) {
-		array_push($buttons, "rve_button");
+	public function registerButtons( $buttons ) {
+		array_push( $buttons, "rve_button" );
+
 		return $buttons;
 	}
 
 	/**
 	 * Function to wrap video into embed-container
 	 */
-	 function registerEmbedHtml($html, $url, $attr, $post_id) {
-		 return '<div class="embed-responsive embed-responsive-16by9">' . $html . '</div>';
-	 }
+	function registerEmbedHtml( $html, $url, $attr, $post_id ) {
+		return '<div class="embed-responsive embed-responsive-16by9">' . $html . '</div>';
+	}
 
- }
+}
 
 Rve::getInstance();
